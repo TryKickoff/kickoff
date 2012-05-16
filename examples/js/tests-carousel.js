e6b3c64cb7832769af75f120572c92fd086b9222
Zander Martineau <zander@martineau.tv>
2012-05-16 18:21:19 +0100

    carousel example & base styles

---
+++ examples/js/tests-carousel.js
@@ -0,0 +1,43 @@
+(function($) {
+
+	$(function() {
+
+		$('.carousel').imagesLoaded( function() {
+			$('.carousel').carouFredSel({
+				width: 600,
+				height: 300,
+				items: {
+					visible: 1,
+					width: 600,
+					height: 300
+				},
+				//auto: false,
+				auto: {
+					pauseDuration: 6000,
+					duration: 500,
+					pauseOnHover: true
+				},
+				pagination: {
+					container: '.carousel-pagination',
+					duration: 300
+				},
+				next: {
+					button: ".carousel-next",
+					key: "right"
+				},
+				prev: {
+					button: ".carousel-prev",
+					key: "left"
+				},
+				scroll: {
+					pauseOnHover: true,
+					fx: 'scroll'
+				}
+			});
+		});//ImagesLoaded
+
+
+	});//End Doc Ready
+
+
+})(jQuery);
