a479dad3900f25ee19bcfaa12fa76dd41cd0580a
Zander Martineau <zander@martineau.tv>
2012-07-12 11:14:56 +0100

    add new pages content

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*	Author:
+		TMW - (Author Name Here)
+*/
+
+
+$(function() {
+	// Any globals go here in CAPS (but avoid if possible)
+
+	// follow a singleton pattern
+	// (http://addyosmani.com/resources/essentialjsdesignpatterns/book/#singletonpatternjavascript)
+	var SiteSetup = {
+		variableX : '', // please don't keep me - only for example syntax!
+
+		init : function () {
+
+		}
+	};
+
+	SiteSetup.init();
+
+});// End Doc Ready
+
