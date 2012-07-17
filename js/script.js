aa1a4e12e348a779c704282c6d5b99fe3c716756
Zander Martineau <zander@martineau.tv>
2012-07-17 17:27:06 +0100

    Create global TMW namespace & add jshint & jslint directives

--- js/script.js
+++ js/script.js
@@ -1,22 +1,49 @@
+/*jslint sloppy: true, white: true, browser: true, devel: true */
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:false, curly:true, browser:true, indent:4, maxerr:50, sloppy:true, white:false, smarttabs:false */
+
 /*	Author:
 		TMW - (Author Name Here)
 */
 
+// ======================================
+// === Declare global 'TMW' namespace ===
+// ======================================
+var TMW = window.TMW || {};
 
-$(function() {
-	// Any globals go here in CAPS (but avoid if possible)
+// Create a closure to maintain scope of the '$' and remain compatible with other frameworks
+(function($) {
 
-	// follow a singleton pattern
-	// (http://addyosmani.com/resources/essentialjsdesignpatterns/book/#singletonpatternjavascript)
-	var SiteSetup = {
-		variableX : '', // please don't keep me - only for example syntax!
+	$(function() {
+		// Any globals go here in CAPS (but avoid if possible)
 
-		init : function () {
+		// follow a singleton pattern
+		// (http://addyosmani.com/resources/essentialjsdesignpatterns/book/#singletonpatternjavascript)
 
-		}
-	};
+		TMW.SiteSetup = {
+			variableX : '', // please don't keep me - only for example syntax!
 
-	SiteSetup.init();
+			init : function () {
 
-});// End Doc Ready
+			}
+		};
+
+		TMW.SiteSetup.init();
+
+	});// END DOC READY
+
+	/* optional triggers
+
+	// WINDOW.LOAD
+	$(window).load(function() {
+
+	});
+
+	// WINDOW.RESIZE
+	$(window).resize(function() {
+
+	});
+
+	*/
+
+})(jQuery);
 
