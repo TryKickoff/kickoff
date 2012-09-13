b74bdd4fa7eac55c240ad2b9ab91c6d031a2b27c
Zander Martineau <zander@martineau.tv>
2012-09-13 12:32:17 +0100

    Add helpers.js

---
+++ js/helpers.js
@@ -0,0 +1,11 @@
+// ==================
+// === HELPERS.js ===
+// ==================
+
+// Array Remove - By John Resig (MIT Licensed)
+// http://ejohn.org/blog/javascript-array-remove/
+Array.prototype.remove = function(from, to) {
+	var rest = this.slice((to || from) + 1 || this.length);
+	this.length = from < 0 ? this.length + from : from;
+	return this.push.apply(this, rest);
+};
