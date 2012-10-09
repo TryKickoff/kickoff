d7b4243721c795f717cbe3c733441699cfe523c6
Zander Martineau <zander@martineau.tv>
2012-10-09 17:15:48 +0100

    Add cookie getter/setter

--- js/helpers.js
+++ js/helpers.js
@@ -11,3 +11,43 @@ if ( !Array.prototype.remove ) {
 		return this.push.apply(this, rest);
 	};
 }
+
+// Complete cookie reader/writer from https://developer.mozilla.org/en-US/docs/DOM/document.cookie
+TMW.cookies = {
+	getItem: function(sKey) {
+		if (!sKey || !this.hasItem(sKey)) { return null; }
+		return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+	},
+	setItem: function(sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+		if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return; }
+		var sExpires = "";
+		if (vEnd) {
+			switch (vEnd.constructor) {
+				case Number:
+					sExpires = vEnd === Infinity ? "; expires=Tue, 19 Jan 2038 03:14:07 GMT" : "; max-age=" + vEnd;
+					break;
+				case String:
+					sExpires = "; expires=" + vEnd;
+					break;
+				case Date:
+					sExpires = "; expires=" + vEnd.toGMTString();
+					break;
+			}
+		}
+		document.cookie = escape(sKey) + "=" + escape(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+	},
+	removeItem: function(sKey, sPath) {
+		if (!sKey || !this.hasItem(sKey)) { return; }
+		document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sPath ? "; path=" + sPath : "");
+	},
+	hasItem: function(sKey) {
+		return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+	},
+	// Optional method: you can safely remove it!
+	keys: function() {
+		var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+		for (var nIdx = 0; nIdx < aKeys.length; nIdx++) { aKeys[nIdx] = unescape(aKeys[nIdx]); }
+		return aKeys;
+	}
+
+};
