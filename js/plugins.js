cf8ddf10ccb4786dcdac7eec81b76320f2ef2f5c
Zander Martineau <zander@martineau.tv>
2013-02-04 18:26:14 +0000

    unminify plugin code

--- js/plugins.js
+++ js/plugins.js
@@ -1,3 +1,6 @@
+/*jslint sloppy: true, white: true, browser: true, devel: true */
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:false, curly:true, browser:true, indent:4, maxerr:50, white:false, smarttabs:false */
+
 /* PLUGIN DIRECTORY
 What you can find in this file [listed in order they appear]
 Please keep me up-to-date :)
@@ -10,11 +13,39 @@ Please keep me up-to-date :)
 
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; try { args.callee = f.caller } catch(e) {}; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
+window.log = function f() {
+		log.history = log.history || [];
+		log.history.push(arguments);
+		if (this.console) {
+				var args = arguments,
+						newarr;
+				try {
+						args.callee = f.caller;
+				} catch (e) {}
+				newarr = [].slice.call(args);
+				if (typeof console.log === 'object')  {
+					log.apply.call(console.log, console, newarr);
+				} else {
+					console.log.apply(console, newarr);
+				}
+		}
+};
 
 // make it safe to use console.log always
-(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
-(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
+(function (a) {
+		function b() {}
+		for (var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","), d; !! (d = c.pop());) {
+				a[d] = a[d] || b;
+		}
+})
+(function () {
+		try {
+				console.log();
+				return window.console;
+		} catch (a) {
+				return (window.console = {});
+		}
+}());
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 // if conflicts wrap in the below
