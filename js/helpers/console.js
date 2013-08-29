f56e18c000860c99d40804ea53e642cb15137f8b
Ashley Nolan <nolly00@gmail.com>
2013-08-29 15:05:10 +0100

    adding in quick log support

--- js/helpers/console.js
+++ js/helpers/console.js
@@ -13,3 +13,24 @@
 		}
 	}
 })(window);
+
+
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function f() {
+	log.history = log.history || [];
+	log.history.push(arguments);
+	if (this.console) {
+		var args = arguments,
+			newarr;
+		try {
+			args.callee = f.caller;
+		} catch (e) {}
+		newarr = [].slice.call(args);
+		if (typeof console.log === 'object')  {
+			log.apply.call(console.log, console, newarr);
+		} else {
+			console.log.apply(console, newarr);
+		}
+	}
+};
