febfdfb5f43655a89dd2e42ff8de513df87b1125
Zander Martineau <zander@martineau.tv>
2013-09-21 11:22:43 +0100

    Improve Gruntfile. Add useful comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,43 @@ module.exports = function (grunt) {
 
 	'use strict';
 
-	// ====================
-	// == Edit this section
+	/* 
+	   Javascript settings - Edit this section
+	   ========================================================================== */
+	/**
+	 * Specify which js files you want to include
+	 */
 	var jsFileList = [
 		'js/helpers/helpers.js',
 		'js/helpers/console.js',
 		'js/script.js'
 	];
-	var distDir = 'js/dist/';
-	var jsFile = 'app.min.js';
-	// ====================
 
-	// Project configuration
+	/**
+	 * Specify your output directory
+	 */
+	var distDir = 'js/dist/';
+	
+	/**
+	 * Specify the name of your compiled JS file
+	 * which will be placed in the directory you define above
+	 */
+	var jsFile = 'app.min.js';
+
+	/* ==================== */
+
+	/**
+	 * Project configuration
+	 */
 	grunt.initConfig({
 		pkg: require('./package'),
 
+
+		/**
+		 * JSHint
+		 * https://github.com/gruntjs/grunt-contrib-jshint
+		 * Manage the options inside .jshintrc file
+		 */
 		jshint: {
 			all: jsFileList,
 			options: {
@@ -24,7 +46,14 @@ module.exports = function (grunt) {
 			}
 		},
 
-		// Choose Sass files below
+
+		/**
+		 * Sass compilation
+		 * https://github.com/gruntjs/grunt-contrib-sass
+		 * Separate options for dev and production environments
+		 * Includes kickoff.scss and kickoff-old-ie.scss by default
+		 * Also creates source maps
+		 */
 		sass: {
 			dev: {
 				options: {
@@ -40,7 +69,7 @@ module.exports = function (grunt) {
 					'css/kickoff-old-ie.css': 'scss/kickoff-old-ie.scss'
 				}
 			},
-			deploy: {
+			production: {
 				options: {
 					style: 'compressed'
 				},
@@ -52,6 +81,13 @@ module.exports = function (grunt) {
 			}
 		},
 
+
+		/**
+		 * Uglify
+		 * https://github.com/gruntjs/grunt-contrib-uglify
+		 * Minifies and concatinates your JS
+		 * Also creates source maps
+		 */
 		uglify: {
 			options: {
 				// mangle: Turn on or off mangling
@@ -76,40 +112,22 @@ module.exports = function (grunt) {
 				// sourceMapPrefix: 1,
 
 			},
+
+			/**
+			 * Use the array at the top of this files to specify which js files you include
+			 */
 			js: {
 				src: jsFileList,
 				dest: distDir + jsFile
 			}
 		},
 
-		//auto prefixes your css using caniuse
-		//https://github.com/ai/autoprefixer
-		autoprefixer: {
-			dist : {
-				options: {
-					// Task-specific options go here - we are supporting
-					// the last 2 browsers, any browsers with >1% market share,
-					// and ensuring we support IE7 + 8 with prefixes
-					browsers: ['last 2 versions', '> 1%', 'ie 8', 'ie 7']
-				},
-				files: {
-					'css/kickoff.prefixed.css': 'css/kickoff.css',
-					'css/kickoff-old-ie.prefixed.css': 'css/kickoff-old-ie.css'
-				}
-			}
-		},
 
-		//https://github.com/t32k/grunt-csso
-		csso: {
-			dist: {
-				files: {
-					'css/kickoff.min.css': ['css/kickoff.prefixed.css'],
-					'css/kickoff-old-ie.min.css': ['css/kickoff-old-ie.prefixed.css']
-				},
-
-			}
-		},
-
+		/**
+		 * Watch
+		 * https://github.com/gruntjs/grunt-contrib-watch
+		 * Watches your scss, js etc for changes and compiles them
+		 */
 		watch: {
 			scss: {
 				files: ['scss/**/*.scss'],
@@ -132,29 +150,82 @@ module.exports = function (grunt) {
 					'css/*.css'
 				]
 			}
+		},
+
+
+		/**
+		 * Autoprefixer
+		 * https://github.com/ai/autoprefixer
+		 * Auto prefixes your CSS using caniuse data
+		 */
+		autoprefixer: {
+			dist : {
+				options: {
+					// Task-specific options go here - we are supporting
+					// the last 2 browsers, any browsers with >1% market share,
+					// and ensuring we support IE7 + 8 with prefixes
+					browsers: ['last 2 versions', '> 1%', 'ie 8', 'ie 7']
+				},
+				files: {
+					'css/kickoff.prefixed.css': 'css/kickoff.css',
+					'css/kickoff-old-ie.prefixed.css': 'css/kickoff-old-ie.css'
+				}
+			}
+		},
+
+
+		/**
+		 * CSSO
+		 * https://github.com/t32k/grunt-csso
+		 * Minify CSS files with CSSO
+		 */
+		csso: {
+			dist: {
+				files: {
+					'css/kickoff.min.css': ['css/kickoff.prefixed.css'],
+					'css/kickoff-old-ie.min.css': ['css/kickoff-old-ie.prefixed.css']
+				},
+
+			}
 		}
 	});
 
 	// Load some stuff
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-csso');
 
-	// =============
-	// === Tasks ===
-	// =============
-	// A task for development
-	grunt.registerTask('dev', ['jshint', 'uglify', 'sass:dev']);
 
-	// A task for deployment
-	grunt.registerTask('deploy', ['jshint', 'uglify', 'sass:deploy']);
-	// grunt.registerTask('deploy', ['jshint', 'uglify', 'sass:deploy', 'autoprefixer', 'csso']);
+	/**
+	 * Available tasks:
+	   * grunt        : run jshint, uglify and sass:dev
+	   * grunt watch  : run sass:dev, uglify and livereload
+	   * grunt dev    : run jshint, uglify and sass:dev
+	   * grunt deploy : run jshint, uglify and sass:production
+	 */
 
+	/**
+	 * Default task
+	 * run jshint, uglify and sass:dev
+	 */
 	// Default task
 	grunt.registerTask('default', ['jshint', 'uglify', 'sass:dev']);
+
+	
+	/**
+	 * A task for development
+	 * run jshint, uglify and sass:dev
+	 */
+	grunt.registerTask('dev', ['jshint', 'uglify', 'sass:dev']);
+
+	/**
+	 * A task for your production environment
+	 * run jshint, uglify and sass:production
+	 */
+	grunt.registerTask('deploy', ['jshint', 'uglify', 'sass:production']);
+	// grunt.registerTask('production', ['jshint', 'uglify', 'sass:production', 'autoprefixer', 'csso']);
 
 };
