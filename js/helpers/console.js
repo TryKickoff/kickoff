b2c028d673ed0d642cfc63b6659fb0338ed45a1c
Zander Martineau <zander@martineau.tv>
2013-12-12 11:21:25 +0000

    remove log and improve info

--- js/helpers/console.js
+++ js/helpers/console.js
@@ -1,5 +1,7 @@
-// Console
-(function (window) {
+/* ==========================================================================
+   Console.log shim
+   ========================================================================== */
+;(function (window) {
 	if (!window.console) {
 		console = {};
 	}
@@ -13,24 +15,3 @@
 		}
 	}
 })(window);
-
-
-// usage: log('inside coolFunc', this, arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function f() {
-	log.history = log.history || [];
-	log.history.push(arguments);
-	if (this.console) {
-		var args = arguments,
-			newarr;
-		try {
-			args.callee = f.caller;
-		} catch (e) {}
-		newarr = [].slice.call(args);
-		if (typeof console.log === 'object')  {
-			log.apply.call(console.log, console, newarr);
-		} else {
-			console.log.apply(console, newarr);
-		}
-	}
-};
