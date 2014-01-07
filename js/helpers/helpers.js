936861afbdafbba60304be74df196b3da043c38d
Zander Martineau <zander@martineau.tv>
2014-01-07 12:06:46 +0000

    Make some fixes after running `grunt jscs`

--- js/helpers/helpers.js
+++ js/helpers/helpers.js
@@ -97,26 +97,35 @@ if (!('create' in Object.prototype)) {
    ========================================================================== */
 Object.compare = function (obj1, obj2) {
 	for (var p in obj1) {
-		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+		if ( obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) {
+			return false;
+		}
 
 		switch (typeof (obj1[p])) {
 			case 'object':
-				if (!Object.compare(obj1[p], obj2[p])) return false;
+				if (!Object.compare(obj1[p], obj2[p])) {
+					return false;
+				}
 				break;
 			case 'function':
-				if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+				if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) {
+					return false;
+				}
 				break;
 			default:
-				if (obj1[p] != obj2[p]) return false;
+				if (obj1[p] != obj2[p]) {
+					return false;
+				}
 		}
 	}
 
 	for (var p in obj2) {
-		if (typeof (obj1[p]) == 'undefined') return false;
+		if (typeof (obj1[p]) == 'undefined') {
+			return false;
+		}
 	}
 	return true;
 };
-
 
 /*
    Function bind patch
@@ -140,4 +149,4 @@ if (!('bind' in Function.prototype)) {
 
 		return fBound;
 	};
-};
+}
