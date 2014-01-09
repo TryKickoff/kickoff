0bc86e54057479265df617a5c70f2c6c189b1d12
Zander Martineau <zander@martineau.tv>
2014-01-09 17:04:35 +0000

    new compiled files

--- js/helpers/helpers.js
+++ js/helpers/helpers.js
@@ -91,41 +91,6 @@ if (!('create' in Object.prototype)) {
 	})();
 }
 
-/*
-   Object comparison is not the same as if you compare primitive types.
-   https://gist.github.com/nicbell/6081098
-   ========================================================================== */
-Object.compare = function (obj1, obj2) {
-	for (var p in obj1) {
-		if ( obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) {
-			return false;
-		}
-
-		switch (typeof (obj1[p])) {
-			case 'object':
-				if (!Object.compare(obj1[p], obj2[p])) {
-					return false;
-				}
-				break;
-			case 'function':
-				if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) {
-					return false;
-				}
-				break;
-			default:
-				if (obj1[p] != obj2[p]) {
-					return false;
-				}
-		}
-	}
-
-	for (var p in obj2) {
-		if (typeof (obj1[p]) == 'undefined') {
-			return false;
-		}
-	}
-	return true;
-};
 
 /*
    Function bind patch
