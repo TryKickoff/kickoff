83d4883adb97bc3904a1250cef72dc01feecd693
Zander Martineau <zander@martineau.tv>
2014-09-06 12:11:36 +0100

    compress styleguide output

--- config/css.js
+++ config/css.js
@@ -24,7 +24,7 @@ module.exports.tasks = {
 		styleguide: {
 			options: {
 				unixNewlines: true,
-				style: 'compact',
+				style: 'compressed',
 				precision : 8,
 				loadPath : 'scss/'
 			},
