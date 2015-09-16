0cb477fc4b0a6a3b894d852d5b612267fdb33cac
Zander Martineau <zander@martineau.tv>
2015-09-17 00:20:55 +0100

    remove jshint, scsslint & photobox grunt plugins

--- _grunt-configs/tests.js
+++ _grunt-configs/tests.js
@@ -1,54 +1,6 @@
 module.exports.tasks = {
 
 	/**
-	 * scss Lint
-	 * https://github.com/ahmednuaman/grunt-scss-lint
-	 */
-	scsslint: {
-		allFiles: [
-			'<%=config.css.scssDir%>/**/*.scss',
-		],
-		options: {
-			config: '.scss-lint.yml',
-			reporterOutput: null
-		},
-	},
-
-
-	/**
-	 * JSHint
-	 * https://github.com/gruntjs/grunt-contrib-jshint
-	 * Manage the options inside .jshintrc file
-	 */
-	jshint: {
-		options: {
-			jshintrc: '.jshintrc'
-		},
-		project: [
-			'<%=config.srcDir%>/js/**/*.js',
-			'!<%=config.srcDir%>/js/{libs,helpers}/**/*.js',
-			'!<%=config.srcDir%>/js/**/*.min.js'
-		]
-	},
-
-
-
-	/**
-	 * Grunt Photobox
-	 * https://github.com/stefanjudis/grunt-photoBox
-	 * Visual regression testing tool
-	 */
-	photobox: {
-		task: {
-			options: {
-			screenSizes : '<%=config.testing.visual.sizes%>',
-			urls        : '<%=config.testing.visual.urls%>'
-			}
-		}
-	},
-
-
-	/**
 	 * Grunt Validation
 	 * https://github.com/praveenvijayan/grunt-html-validation
 	 * W3C html validation grunt plugin
