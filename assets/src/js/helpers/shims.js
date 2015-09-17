88e1fbfa37f32f739e6f2f3d7a378183affab91a
Zander Martineau <zander@martineau.tv>
2015-09-17 01:08:38 +0100

    new shims build based on new criteria

--- assets/src/js/helpers/shims.js
+++ assets/src/js/helpers/shims.js
@@ -1,46 +1,45 @@
 /*! 
-Included shims: Array.forEach,Array.filter,Array.map,Function.bind,EventListener
+Included shims: Array.forEach,EventListener
 */
 
-/*
-    Array.prototype.forEach()
-*/
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.io/#x15.4.4.18
 if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function(t, e) {
+    Array.prototype.forEach = function(e, t) {
         var n, r;
         if (this == null) {
             throw new TypeError(" this is null or not defined");
         }
         // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
-        var i = Object(this);
+        var a = Object(this);
         // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
         // 3. Let len be ToUint32(lenValue).
-        var o = i.length >>> 0;
+        var i = a.length >>> 0;
         // 4. If IsCallable(callback) is false, throw a TypeError exception.
         // See: http://es5.github.com/#x9.11
-        if (typeof t !== "function") {
-            throw new TypeError(t + " is not a function");
+        if (typeof e !== "function") {
+            throw new TypeError(e + " is not a function");
         }
         // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-        if (e) {
-            n = e;
+        if (arguments.length > 1) {
+            n = t;
         }
         // 6. Let k be 0
         r = 0;
         // 7. Repeat, while k < len
-        while (r < o) {
-            var a;
+        while (r < i) {
+            var o;
             // a. Let Pk be ToString(k).
             //   This is implicit for LHS operands of the in operator
             // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
             //   This step can be combined with c
             // c. If kPresent is true, then
-            if (r in i) {
+            if (r in a) {
                 // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
-                a = i[r];
+                o = a[r];
                 // ii. Call the Call internal method of callback with T as the this value and
                 // argument list containing kValue, k, and O.
-                t.call(n, a, r, i);
+                e.call(n, o, r, a);
             }
             // d. Increase k by 1.
             r++;
@@ -48,105 +47,44 @@ if (!Array.prototype.forEach) {
     };
 }
 
-/*
-    Array.prototype.filter()
-*/
-if (!Array.prototype.filter) {
-    Array.prototype.filter = function(t) {
-        "use strict";
-        if (this === void 0 || this === null) throw new TypeError();
-        var e = Object(this);
-        var n = e.length >>> 0;
-        if (typeof t !== "function") throw new TypeError();
-        var r = [];
-        var i = arguments.length >= 2 ? arguments[1] : void 0;
-        for (var o = 0; o < n; o++) {
-            if (o in e) {
-                var a = e[o];
-                // NOTE: Technically this should Object.defineProperty at
-                //       the next index, as push can be affected by
-                //       properties on Object.prototype and Array.prototype.
-                //       But that method's new, and collisions should be
-                //       rare, so use the more-compatible alternative.
-                if (t.call(i, a, o, e)) r.push(a);
-            }
-        }
-        return r;
-    };
-}
-
-/*
-   Array map patch
-   ========================================================================== */
-if (!("map" in Array.prototype)) {
-    Array.prototype.map = function(t, e) {
-        var n = this.length >>> 0, r = Array(n);
-        for (var i = 0; i < n; i++) {
-            if (i in this) {
-                r[i] = t.call(e, this[i], i, this);
-            }
-        }
-        return r;
-    };
-}
-
-/*
-    Function.prototype.bind()
-*/
-if (!Function.prototype.bind) {
-    Function.prototype.bind = function(t) {
-        if (typeof this !== "function") {
-            // closest thing possible to the ECMAScript 5
-            // internal IsCallable function
-            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-        }
-        var e = Array.prototype.slice.call(arguments, 1), n = this, r = function() {}, i = function() {
-            return n.apply(this instanceof r && t ? this : t, e.concat(Array.prototype.slice.call(arguments)));
-        };
-        r.prototype = this.prototype;
-        i.prototype = new r();
-        return i;
-    };
-}
-
-// EventListener | MIT/GPL2 | https://github.com/jonathantneal/EventListener
+// EventListener | CC0 | github.com/jonathantneal/EventListener
 this.Element && Element.prototype.attachEvent && !Element.prototype.addEventListener && function() {
-    function t(t, e) {
-        Window.prototype[t] = HTMLDocument.prototype[t] = Element.prototype[t] = e;
+    function e(e, t) {
+        Window.prototype[e] = HTMLDocument.prototype[e] = Element.prototype[e] = t;
     }
     // add
-    t("addEventListener", function(t, e) {
-        var n = this, r = n.addEventListener.listeners = n.addEventListener.listeners || {}, i = r[t] = r[t] || [];
+    e("addEventListener", function(e, t) {
+        var n = this, r = n.addEventListener.listeners = n.addEventListener.listeners || {}, a = r[e] = r[e] || [];
         // if no events exist, attach the listener
-        if (!i.length) {
-            n.attachEvent("on" + t, i.event = function(t) {
-                var e = n.document && n.document.documentElement || n.documentElement || {
+        if (!a.length) {
+            n.attachEvent("on" + e, a.event = function(e) {
+                var t = n.document && n.document.documentElement || n.documentElement || {
                     scrollLeft: 0,
                     scrollTop: 0
                 };
                 // polyfill w3c properties and methods
-                t.currentTarget = n;
-                t.pageX = t.clientX + e.scrollLeft;
-                t.pageY = t.clientY + e.scrollTop;
-                t.preventDefault = function() {
-                    t.returnValue = false;
+                e.currentTarget = n;
+                e.pageX = e.clientX + t.scrollLeft;
+                e.pageY = e.clientY + t.scrollTop;
+                e.preventDefault = function() {
+                    e.returnValue = false;
                 };
-                t.relatedTarget = t.fromElement || null;
-                t.stopImmediatePropagation = function() {
+                e.relatedTarget = e.fromElement || null;
+                e.stopImmediatePropagation = function() {
                     l = false;
-                    t.cancelBubble = true;
+                    e.cancelBubble = true;
                 };
-                t.stopPropagation = function() {
-                    t.cancelBubble = true;
+                e.stopPropagation = function() {
+                    e.cancelBubble = true;
                 };
-                t.target = t.srcElement || n;
-                t.timeStamp = +new Date();
+                e.target = e.srcElement || n;
+                e.timeStamp = +new Date();
                 // create an cached list of the master events list (to protect this loop from breaking when an event is removed)
-                for (var r = 0, o = [].concat(i), a, l = true; l && (a = o[r]); ++r) {
+                for (var r = 0, i = [].concat(a), o, l = true; l && (o = i[r]); ++r) {
                     // check to see if the cached event still exists in the master events list
-                    for (var c = 0, s; s = i[c]; ++c) {
-                        if (s == a) {
-                            s.call(n, t);
+                    for (var c = 0, s; s = a[c]; ++c) {
+                        if (s == o) {
+                            s.call(n, e);
                             break;
                         }
                     }
@@ -154,31 +92,31 @@ this.Element && Element.prototype.attachEvent && !Element.prototype.addEventList
             });
         }
         // add the event to the master event list
-        i.push(e);
+        a.push(t);
     });
     // remove
-    t("removeEventListener", function(t, e) {
-        var n = this, r = n.addEventListener.listeners = n.addEventListener.listeners || {}, i = r[t] = r[t] || [];
+    e("removeEventListener", function(e, t) {
+        var n = this, r = n.addEventListener.listeners = n.addEventListener.listeners || {}, a = r[e] = r[e] || [];
         // remove the newest matching event from the master event list
-        for (var o = i.length - 1, a; a = i[o]; --o) {
-            if (a == e) {
-                i.splice(o, 1);
+        for (var i = a.length - 1, o; o = a[i]; --i) {
+            if (o == t) {
+                a.splice(i, 1);
                 break;
             }
         }
         // if no events exist, detach the listener
-        if (!i.length && i.event) {
-            n.detachEvent("on" + t, i.event);
+        if (!a.length && a.event) {
+            n.detachEvent("on" + e, a.event);
         }
     });
     // dispatch
-    t("dispatchEvent", function(t) {
-        var e = this, n = t.type, r = e.addEventListener.listeners = e.addEventListener.listeners || {}, i = r[n] = r[n] || [];
+    e("dispatchEvent", function(e) {
+        var t = this, n = e.type, r = t.addEventListener.listeners = t.addEventListener.listeners || {}, a = r[n] = r[n] || [];
         try {
-            return e.fireEvent("on" + n, t);
-        } catch (o) {
-            if (i.event) {
-                i.event(t);
+            return t.fireEvent("on" + n, e);
+        } catch (i) {
+            if (a.event) {
+                a.event(e);
             }
             return;
         }
@@ -186,37 +124,37 @@ this.Element && Element.prototype.attachEvent && !Element.prototype.addEventList
     // CustomEvent
     Object.defineProperty(Window.prototype, "CustomEvent", {
         get: function() {
-            var t = this;
-            return function e(n, r) {
-                var i = t.document.createEventObject(), o;
-                i.type = n;
-                for (o in r) {
-                    if (o == "cancelable") {
-                        i.returnValue = !r.cancelable;
-                    } else if (o == "bubbles") {
-                        i.cancelBubble = !r.bubbles;
-                    } else if (o == "detail") {
-                        i.detail = r.detail;
+            var e = this;
+            return function t(n, r) {
+                var a = e.document.createEventObject(), i;
+                a.type = n;
+                for (i in r) {
+                    if (i == "cancelable") {
+                        a.returnValue = !r.cancelable;
+                    } else if (i == "bubbles") {
+                        a.cancelBubble = !r.bubbles;
+                    } else if (i == "detail") {
+                        a.detail = r.detail;
                     }
                 }
-                return i;
+                return a;
             };
         }
     });
     // ready
-    function e(t) {
-        if (e.interval && document.body) {
-            e.interval = clearInterval(e.interval);
+    function t(e) {
+        if (t.interval && document.body) {
+            t.interval = clearInterval(t.interval);
             document.dispatchEvent(new CustomEvent("DOMContentLoaded"));
         }
     }
-    e.interval = setInterval(e, 1);
-    window.addEventListener("load", e);
+    t.interval = setInterval(t, 1);
+    window.addEventListener("load", t);
 }();
 
 !this.CustomEvent && function() {
     // CustomEvent for browsers which don't natively support the Constructor method
-    window.CustomEvent = function t(e, n) {
+    window.CustomEvent = function e(t, n) {
         var r;
         n = n || {
             bubbles: false,
@@ -225,11 +163,11 @@ this.Element && Element.prototype.attachEvent && !Element.prototype.addEventList
         };
         try {
             r = document.createEvent("CustomEvent");
-            r.initCustomEvent(e, n.bubbles, n.cancelable, n.detail);
-        } catch (i) {
+            r.initCustomEvent(t, n.bubbles, n.cancelable, n.detail);
+        } catch (a) {
             // for browsers which don't support CustomEvent at all, we use a regular event instead
             r = document.createEvent("Event");
-            r.initEvent(e, n.bubbles, n.cancelable);
+            r.initEvent(t, n.bubbles, n.cancelable);
             r.detail = n.detail;
         }
         return r;
