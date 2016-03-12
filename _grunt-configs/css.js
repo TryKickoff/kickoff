b30dcaaa35447a94a3310eb29dae7ca4cf080c9b
Zander Martineau <zander@martineau.tv>
2016-03-12 20:03:44 +0000

    fixes indentation

--- _grunt-configs/css.js
+++ _grunt-configs/css.js
@@ -36,7 +36,9 @@ module.exports = function (grunt, options) {
 				from: scssDir + '/styleguide.scss',
 				to: cssDir + '/styleguide.css'
 			}
-		]).then(done).catch(function(error) {
+		])
+		.then(done)
+		.catch(function(error) {
 			console.error('\n' + error.formatted + '\n');
 		});
 	});
