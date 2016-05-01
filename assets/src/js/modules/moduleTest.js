a278d02377ba10bdc395f49ed3592b166997e1d0
Zander Martineau <zander@martineau.tv>
2016-05-01 10:32:03 +0100

    fixes eslint errors

--- assets/src/js/modules/moduleTest.js
+++ assets/src/js/modules/moduleTest.js
@@ -2,16 +2,15 @@
 	moduleTest.js
 	Example module to show how to include other JS files into you browserify build
 */
-'use strict';
 
 // dependencies for this module go here
 
-function init() {
-	doSomething();
-}
-
 function doSomething() {
 	console.log('Something was done :)');
+}
+
+function init() {
+	doSomething();
 }
 
 module.exports = init;
