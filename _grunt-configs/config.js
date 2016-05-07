eee0d88e59c90146137c9c7dd7f4372310674b7b
Zander Martineau <zander@martineau.tv>
2016-05-07 02:21:27 +0100

    reorder contents

--- _grunt-configs/config.js
+++ _grunt-configs/config.js
@@ -4,7 +4,8 @@
  */
 
 module.exports = {
-	src: './_grunt-configs/*.js', // This directory. Has all the Grunt tasks grouped into specific js files
+	src: './_grunt-configs/*.js',
+	// The current directory. It has all the Grunt tasks grouped into specific js files
 
 	srcDir: 'assets/src',   // <%=config.srcDir%>
 	distDir: 'assets/dist', // <%=config.distDir%>
@@ -16,13 +17,13 @@ module.exports = {
 		scssDir: '<%=config.srcDir%>/scss', // <%=config.css.scssDir%>
 		distDir: '<%=config.distDir%>/css', // <%=config.css.distDir%>
 
+		distFile: 'kickoff', // <%=config.css.distFile%>
 		// Renaming this changes the name of the generated CSS file
 		// Make sure you update your template file
-		distFile: 'kickoff', // <%=config.css.distFile%>
 
+		autoprefixer: ['> 5%', 'last 2 versions', 'ie > 8'], // <%=config.css.autoprefixer%>
 		// We are supporting the last 2 browsers, any browsers with >5% market share,
 		// and ensuring we support IE8+ with prefixes
-		autoprefixer: ['> 5%', 'last 2 versions', 'ie > 8'], // <%=config.css.autoprefixer%>
 	},
 
 
@@ -31,9 +32,9 @@ module.exports = {
 		srcFile: '<%=config.srcDir%>/js/script.js', // <%=config.js.srcFile%>
 		distDir: '<%=config.distDir%>/js/',         // <%=config.js.distDir%>
 
+		distFile: 'bundle.js', // <%=config.js.distFile%>
 		// Renaming this changes the name of the generated JS file
 		// Make sure you update your template file
-		distFile: 'bundle.js', // <%=config.js.distFile%>
 	},
 
 
