30d705fbba85ee9ffa46a6d8719c0d3a0d2797f6
JP Driver <jpsdriver@gmail.com>
2016-06-15 17:33:03 +0100

    ditch webpack entry points we don't need

--- assets/src/entry-kickoff.js
+++
@@ -1,9 +0,0 @@
-// kickoff src
-import './scss/kickoff.scss';
-import './js/script.js';
-import './img/icons';
-
-// standalone files
-import './js/standalone/modernizr.js';
-import './js/standalone/README.md';
-import './js/standalone/shims.js';
--- assets/src/entry-styleguide.js
+++
@@ -1,3 +0,0 @@
-// styleguide
-// HACK -- for some reason Webpack doesn't work importing styleguide.scss directly
-import './scss/styleguide-webpack.scss';
