f6b790eb524bb4b93dc1ac4e07cec06966eaeeda
Zander Martineau <zander@martineau.tv>
2016-08-09 10:42:25 +0100

    fix: fixes and improves image compression
    imagemin-mozjpeg package proved to be troublesome on some environments and SVG compression was added
    
    using SVGO

--- _grunt-configs/images.js
+++ _grunt-configs/images.js
@@ -1,5 +1,5 @@
 var pngquant = require('imagemin-pngquant');
-var mozjpeg = require('imagemin-mozjpeg');
+var svgo = require('imagemin-svgo');
 
 module.exports.tasks = {
 
@@ -13,16 +13,30 @@ module.exports.tasks = {
 			options: {
 				optimizationLevel: 3,
 				progressive: true,
+				svgoPlugins: [
+					{ cleanupAttrs: true },
+					{ removeComments: true },
+					{ removeDoctype: true },
+					{ removeMetadata: true },
+					{ removeXMLProcInst: true },
+					{ removeEditorsNSData: true },
+					{ cleanupNumericValues: true },
+					{ collapseGroups: true },
+					{ sortAttrs: true },
+					{ removeViewBox: false },
+					{ removeUselessStrokeAndFill: false },
+					{ removeEmptyAttrs: false },
+				],
 				use: [
 					pngquant({ quality: '40-50', speed: 4 }),
-					mozjpeg({ quality: 70 }),
+					svgo(),
 				],
 			},
 
 			files: [{
 				expand: true,
 				cwd: '<%=config.img.srcDir%>/',
-				src: ['**/*.{svg,png,jpg,gif}'],
+				src: ['**/*.*'],
 				dest: '<%=config.img.distDir%>',
 			}],
 		},
