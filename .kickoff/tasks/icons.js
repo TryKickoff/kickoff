5afeee6be7e417286e4ca5c68602c10a746ece6e
Zander Martineau <zander@martineau.tv>
2016-08-29 23:07:27 +0100

    feat: [gulp] add more detailed svgo options when compressing icons

--- .kickoff/tasks/icons.js
+++ .kickoff/tasks/icons.js
@@ -7,12 +7,31 @@ const config = require('../config');
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const svgstore = require('gulp-svgstore');
+var svgo = require('imagemin-svgo');
 
 
 gulp.task('icons', () => {
 	return gulp.src([`${config.icons.srcDir}/*`])
 		.pipe(
-			imagemin()
+			imagemin([
+				imagemin.svgo({
+					plugins: [
+						{ cleanupAttrs: true },
+						{ removeComments: true },
+						{ removeDoctype: true },
+						{ removeMetadata: true },
+						{ removeXMLProcInst: true },
+						{ removeEditorsNSData: true },
+						{ cleanupNumericValues: true },
+						{ collapseGroups: true },
+						{ sortAttrs: true },
+						{ removeViewBox: false },
+						{ removeUselessStrokeAndFill: false },
+						{ removeEmptyAttrs: false },
+					]
+				})
+			],
+			{verbose: true})
 		)
 		.pipe(
 			svgstore()
