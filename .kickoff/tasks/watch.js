9d27a4b29ff937d208cf8756f864f4c46f51fc3a
Zander Martineau <zander@martineau.tv>
2016-08-29 11:39:55 +0100

    rename task and add alias: `gulp watch` === `gulp watcher`

--- .kickoff/tasks/watch.js
+++ .kickoff/tasks/watch.js
@@ -1,14 +1,16 @@
 /**
  * gulp watcher
  */
-
 const config = require('../config');
 const gulp = require('gulp');
 
-gulp.task('watcher', ['compile'], () => {
+gulp.task('watch', ['compile'], () => {
 	gulp.watch([`${config.css.scssDir}/**/*.scss`], ['css']);
 	gulp.watch([`${config.icons.srcDir}/**/*`], ['icons']);
 	gulp.watch([`${config.img.srcDir}/**/*`], ['images']);
 	gulp.watch(`${config.js.srcDir}/**/*.js`, ['javascript']);
 });
+
+// Alias of watch task
+gulp.task('watcher', ['watch'])
 
