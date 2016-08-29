a3d502070fe1d099c2b4b76dc61425fb8d63db03
Zander Martineau <zander@martineau.tv>
2016-08-29 11:57:48 +0100

    chore: improve readability

--- .kickoff/tasks/compile.js
+++ .kickoff/tasks/compile.js
@@ -9,4 +9,9 @@ const gulp = require('gulp');
 /**
  * gulp compile
  */
-gulp.task('compile', ['css', 'images', 'icons', 'javascript']);
+gulp.task('compile', [
+	'css',
+	'javascript',
+	'images',
+	'icons'
+]);
