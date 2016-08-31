0262206d856fe7943635bb926fb3156e1c71f683
Zander Martineau <zander@martineau.tv>
2016-08-31 12:24:32 +0100

    chore: tidy & remove redundant code

--- .kickoff/index.js
+++ .kickoff/index.js
@@ -14,13 +14,13 @@
  */
 
 const fs = require('fs');
-const gulp = require('gulp');
+
 const tasksDirectory = `${__dirname}/tasks/`;
 const tasks = fs.readdirSync(tasksDirectory);
 
 // loop through .kickoff and require each
 // any tasks you add will be included automatically
-tasks.forEach((task) => {
+tasks.forEach(task => {
 	// only add .js files
 	if (task.match(/\.js$/)) {
 		require(`${tasksDirectory}/${task}`);
