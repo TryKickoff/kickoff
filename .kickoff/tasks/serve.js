e38831b0e7eabe6a22cf56e2738b8d9612ab8f1f
Zander Martineau <zander@martineau.tv>
2016-09-01 15:54:15 +0100

    replace existing `gulp serve` task with this one.

--- .kickoff/tasks/serve.js
+++ .kickoff/tasks/serve.js
@@ -1,36 +1,71 @@
 /**
- * gulp javascript
- * Gulp task to compile js (no minification)
+ * gulp serve
+ * Gulp task to run Browsersync server
  */
-
 const gulp = require('gulp');
-const gutil = require('gulp-util');
+const browserSync = require('browser-sync').create();
+const path = require('path');
 const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('../config');
 const webpackConfig = require('../config/webpack.config.js');
-const WebpackDevServer = require('webpack-dev-server');
 
+// const reload = browserSync.reload;
+const compiler = webpack(webpackConfig);
 
-gulp.task('serve', () => {
-	var myConfig = Object.create(webpackConfig);
-	// eval each module (no source maps)
-	myConfig.devtool = 'eval';
-	myConfig.debug = true;
+gulp.task('serve', ['watcher'], () => {
 
-	myConfig.plugins.push(
-		new webpack.HotModuleReplacementPlugin()
+	webpackConfig.plugins.push(
+		// Webpack 1.0
+		new webpack.optimize.OccurenceOrderPlugin(),
+		// Webpack 2.0 fixed this mispelling
+		// new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
 	);
 
-	myConfig.entry.kickoff.unshift("webpack-dev-server/client?http://localhost:8080/", "webpack/hot/dev-server");
+	for (const key in config.js.entryPoints) {
+		config.js.entryPoints[key].unshift('webpack-hot-middleware/client?&reload=true');
+	}
 
-	// run webpack-dev-server
-	new WebpackDevServer(webpack(myConfig), {
-		publicPath: myConfig.output.publicPath,
-		hot: true,
-		stats: {
-			colors: true
+	webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+	browserSync.init({
+		server: {
+			baseDir: './',
 		},
-	}).listen(3000, 'localhost', (err) => {
-		if (err) throw new gutil.PluginError('webpack-dev-server', err);
-		gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+
+		middleware: [
+			webpackDevMiddleware(compiler, {
+				stats: 'errors-only',
+				publicPath: path.resolve(webpackConfig.output.publicPath),
+			}),
+			webpackHotMiddleware(compiler),
+		],
+
+		files: [
+			`${config.css.distDir}/**/*.css`,
+			// `${config.js.distDir}/**/*.js`,
+			`${config.img.distDir}/**/*`,
+			`${config.icons.distDir}/**/*`,
+			'**/*.html',
+		],
+
+		notify: {
+			styles: [
+				'pointer-events: none',
+				'position: fixed',
+				'bottom: 10px',
+				'left: 10px',
+				'text-align: center',
+				'background-color: rgba(24, 24, 48, 0.8)',
+				'color: #fff',
+				'padding: 10px',
+				'border: 2px solid #fff',
+				'font: 13px Menlo, Monaco, monospace',
+				'border-radius: 5px',
+			],
+		},
 	});
 });
