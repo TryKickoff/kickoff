e4aaea0222942ab7764740ab6af5d699f2adf483
Zander Martineau <zander@martineau.tv>
2016-10-23 19:00:45 +0100

    build(config): reorder comments above their items

--- .kickoff/config.js
+++ .kickoff/config.js
@@ -18,13 +18,13 @@ const ConfigOptions = function () {
 		scssDir: `${config.srcDir}/scss`, // config.css.scssDir
 		distDir: `${config.distDir}/css`, // config.css.distDir
 
-		distFile: 'kickoff', // config.css.distFile
 		// Renaming this changes the name of the generated CSS file
 		// Make sure you update your template file
+		distFile: 'kickoff', // config.css.distFile
 
-		browsers: ['> 5%', 'last 2 versions', 'ie > 8'], // config.css.browsers
 		// We are supporting the last 2 browsers, any browsers with >5% market share,
 		// and ensuring we support IE9+ with prefixes
+		browsers: ['> 5%', 'last 2 versions', 'ie > 8'], // config.css.browsers
 	};
 
 	// Javascript-related vars
@@ -41,9 +41,10 @@ const ConfigOptions = function () {
 		},
 
 		distDir: `${config.distDir}/js`, // config.js.distDir
-		distFile: 'kickoff.js', // config.js.distFile
+
 		// Renaming this changes the name of the generated JS file
 		// Make sure you update your template file
+		distFile: 'kickoff.js', // config.js.distFile
 	};
 
 	// Image-related vars
