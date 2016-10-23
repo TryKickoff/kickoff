38a4f2919fb55966a1dad2be44cb9dc5f20e79f5
Zander Martineau <zander@martineau.tv>
2016-10-24 00:11:30 +0100

    tidy main javascript file

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -6,8 +6,6 @@
  * Need help using JSDoc? Find out more at http://usejsdoc.org/
  */
 
-'use-strict';
-
 // their code e.g. npm modules
 import ready from 'lite-ready'; // https://github.com/nicbell/liteready
 import svg4everybody from 'svg4everybody'; // https://github.com/jonathantneal/svg4everybody
@@ -17,11 +15,13 @@ import 'console';
 import 'kickoff-welcome.js'; // The Kickoff message in the js console. Remove it if you like :)
 
 // our code
-// this is a test, uncomment the line below to try it
-// import moduleTest from './modules/module-test';
+// import moduleTest from './modules/module-test'; // this is a test, uncomment the line below to try it
 
 // DOM ready code goes in here
 ready(() => {
+	svg4everybody({
+		polyfill: true, // polyfill <use> elements for External Content
+	});
+
 	// moduleTest(); // this is a test, uncomment this line to try it
-	svg4everybody();
 });
