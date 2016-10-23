be8e14606953448bcb028dc7eb2055c5fd1f7828
Zander Martineau <zander@martineau.tv>
2016-10-23 18:58:12 +0100

    improve console output when using `gulp compile`
    now notifies user when `RELEASE` flag is being used

--- .kickoff/tasks/compile.js
+++ .kickoff/tasks/compile.js
@@ -5,9 +5,22 @@ const gulp = require('gulp');
 const config = require('../config');
 
 gulp.task('compile', [
+	'precompile',
 	'css',
 	'javascript',
 	'images',
 	'svg',
 	'copy',
-]);
+], () => {
+	console.log();
+	console.log('❯❯ Kickoff compiled');
+	console.log();
+});
+
+gulp.task('precompile', () => {
+	if (process.env.RELEASE) {
+		console.log();
+		console.log('❯❯ Creating an optimized production build...');
+		console.log();
+	}
+});
