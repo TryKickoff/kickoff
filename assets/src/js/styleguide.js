2d18d83990684810328628e12fa88939473e25e3
Zander Martineau <zander@martineau.tv>
2017-01-01 02:21:52 +0000

    styleguide: update content & allow copying of some values

---
+++ assets/src/js/styleguide.js
@@ -0,0 +1,34 @@
+import ready from 'lite-ready';
+import $$ from 'double-dollar';
+import copy from 'copy-js/src/copy';
+
+ready(() => {
+	copyText();
+});
+
+/**
+ * copyText
+ *
+ * Usage:
+ * add `data-copy="your text"` to any html element on the styleguide
+ */
+function copyText() {
+	$$('[data-copy]').forEach(item => {
+		item.addEventListener('click', e => {
+			e.preventDefault();
+			e.stopPropagation();
+			const target = e.currentTarget;
+			const copyValue = target.getAttribute('data-copy');
+			target.classList.add('is-active');
+
+			copy(copyValue, () => {
+				console.log('copied ', copyValue);
+
+				setTimeout(() => {
+					target.classList.remove('is-active');
+				}, 500);
+			});
+		}, false);
+	});
+}
+
