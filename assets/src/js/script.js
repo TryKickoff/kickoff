158501032f9f69a05f2c29bac0d0429ec40c3b8e
Zander Martineau <zander@martineau.tv>
2017-01-01 01:45:04 +0000

    js: update main script file
    ## Added:
    - svg4everybody polyfill is needed to provide consistent icon experience across all browsers
    - kickoff-welcome.js, see https://github.com/TryKickoff/kickoff-welcome.js
    
    ## Removed:
    - double-dollar is not needed for every project so it was removed (it is still needed for the styleguide though)

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,27 +1,27 @@
 /**
- * Project Name:
- * @description
- * @author
+ * Project Name: Kickoff
+ * @description The Kickoff boilerplate js file
+ * @author The Kickoff team
  *
  * Need help using JSDoc? Find out more at http://usejsdoc.org/
  */
 
-'use-strict';
-
 // their code e.g. npm modules
-import ready from 'lite-ready';
-import $$ from 'double-dollar'; // https://github.com/mrmartineau/double-dollar
+import ready from 'lite-ready'; // https://github.com/nicbell/liteready
+import svg4everybody from 'svg4everybody'; // https://github.com/jonathantneal/svg4everybody
 
-// Bundle global libs that don't return a value
+// Global libs that don't return a value
 import 'console';
+import 'kickoff-welcome.js'; // The Kickoff message in the js console. Remove it if you like :)
 
 // our code
-// this is a test, uncomment the line below to try it
-// import moduleTest from './modules/module-test';
-
-window.$$ = $$; // add double-dollar to global scope
+// import moduleTest from './modules/module-test'; // this is a test, uncomment the line below to try it
 
 // DOM ready code goes in here
 ready(() => {
+	svg4everybody({
+		polyfill: true, // polyfill <use> elements for External Content
+	});
+
 	// moduleTest(); // this is a test, uncomment this line to try it
 });
