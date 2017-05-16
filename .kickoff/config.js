bc832f7589108ca67da02891e8b3da5d4aad6bfa
Zander Martineau <zander@martineau.tv>
2017-05-16 08:01:36 +0100

    update the browserslist to be more relevant

--- .kickoff/config.js
+++ .kickoff/config.js
@@ -24,7 +24,7 @@ const ConfigOptions = function () {
 
 		// We are supporting the last 2 browsers, any browsers with >5% market share,
 		// and ensuring we support IE9+ with prefixes
-		browsers: ['> 5%', 'last 2 versions', 'ie > 8'], // config.css.browsers
+		browsers: ['> 1%', 'last 1 versions', 'ie > 9', 'not ie <= 8', 'edge >= 12'], // config.css.browsers
 	};
 
 	// Javascript-related vars
